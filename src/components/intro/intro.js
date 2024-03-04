bbbb8a370d056e44600cef353fec56a581ab186d
Lakshya-mogha <143194300+Lakshya-mogha@users.noreply.github.com>
2024-03-04 16:56:33 +0530

    portfolio

---
+++ src/components/intro/intro.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./intro.css"
+
+function Intro() {
+  return (
+    <div className='md:flex'>
+      <div className='mt-[197px] md:ml-[197px] text-[1.6rem] ml-5'>
+      <h1 className='font-bold'>नमस्कार🙏 </h1>
+      <p className=''>
+        I'm Lakshya Mogha<br />
+        A Full Stack Web Developer.
+      </p>
+      </div>
+      <img src="./img/OIcVN0R7Yxstun0LPdOkl-transformed-removebg-preview.png" alt="" className='blob md:ml-[180px] md:mt-[100px] mt-10'/>
+    </div>
+  )
+}
+
+export default Intro
---
+++ src/components/projects/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div className='p-4'>
+      <hr className='border-1 border-black' />
+      <h1 className='font-bold'>Projects</h1>
+    </div>
+  )
+}
+
+export default Projects
