7e6f0c391c36389ea41c69be6cf94a4ae07dcf99
Lakshya-mogha <143194300+Lakshya-mogha@users.noreply.github.com>
2024-05-01 03:10:58 +0530

    font changes

--- src/components/footer/contactForm.js
+++ src/components/footer/contactForm.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function ContactForm() {
   return (
-    <div className="grid grid-flow-row gap-4">
+    <div className="grid grid-flow-row gap-4 font-Lora">
         Name
-      <label className="input input-bordered flex items-center ">
+      <label className="input input-bordered flex items-center font-Inter ">
         <input type="text" className="grow" placeholder="" />
       </label>
         Email 
-      <label className="input input-bordered flex items-center ">
+      <label className="input input-bordered flex items-center font-Inter">
         <input type="text" className="grow" placeholder="example@example.com" />
       </label>
       <textarea
-        className="textarea textarea-bordered"
+        className="textarea textarea-bordered font-Inter"
         placeholder="connect with me "
       ></textarea>
       <button className="btn btn-outline">Submit</button>
