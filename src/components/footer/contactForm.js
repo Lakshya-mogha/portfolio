fb26df1eb5aa99aab3a05f32f4060d87dee6dcc5
Lakshya-mogha <143194300+Lakshya-mogha@users.noreply.github.com>
2024-04-30 17:05:58 +0530

    made contact form

--- src/components/footer/contactForm.js
+++ src/components/footer/contactForm.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function ContactForm() {
   return (
-    <div>
-      contact form 
+    <div className="grid grid-flow-row gap-4">
+        Name
+      <label className="input input-bordered flex items-center ">
+        <input type="text" className="grow" placeholder="" />
+      </label>
+        Email 
+      <label className="input input-bordered flex items-center ">
+        <input type="text" className="grow" placeholder="example@example.com" />
+      </label>
+      <textarea
+        className="textarea textarea-bordered"
+        placeholder="connect with me "
+      ></textarea>
+      <button className="btn btn-outline">Submit</button>
     </div>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
