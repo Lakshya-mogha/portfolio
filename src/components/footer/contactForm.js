0a41f8a118ff7a3bb05efd5afdd4a4ec02956833
lakshya <lakshyamogha1@gmail.com>
2024-08-12 16:57:02 +0530

    form submit functionality

--- src/components/footer/contactForm.js
+++ src/components/footer/contactForm.js
@@ -1,20 +1,41 @@
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 function ContactForm() {
+  const [state, handleSubmit] = useForm("xblrypvr");
+  if (state.succeeded) {
+    return <p>Thanks for your message</p>;
+  }
   return (
-    <div className="grid grid-flow-row gap-4 font-Lora">
-      <label className="input input-bordered flex items-center font-Inter ">
-        <input type="text" className="grow" placeholder="Name" />
-      </label>
-      <label className="input input-bordered flex items-center font-Inter">
-        <input type="text" className="grow" placeholder="Email" />
-      </label>
-      <textarea
-        className="textarea textarea-bordered font-Inter"
-        placeholder="your message"
-      ></textarea>
-      <button className="btn btn-outline">Submit</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="grid grid-flow-row gap-4 font-Lora">
+        <label className="input input-bordered flex items-center font-Inter ">
+          <input type="text" className="grow" placeholder="Name" name="name" />
+        </label>
+        <label className="input input-bordered flex items-center font-Inter">
+          <input
+            type="text"
+            className="grow"
+            placeholder="Email"
+            name="email"
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </label>
+        <textarea
+          className="textarea textarea-bordered font-Inter"
+          placeholder="your message"
+          name="message"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button className="btn btn-outline" disabled={state.submitting}>
+          Submit
+        </button>
+      </div>
+    </form>
   );
 }
 
